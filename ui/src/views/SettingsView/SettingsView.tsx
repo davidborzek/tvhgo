5952211a49cdc6ea633ffc0e17f034a2c6830862
David Borzek <mail@davidborzek.de>
2023-01-27 00:04:21 +0100

    feat: change password in settings ui

--- ui/src/views/SettingsView/SettingsView.tsx
+++ ui/src/views/SettingsView/SettingsView.tsx
@@ -1,16 +1,26 @@
+import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Dropdown, { Option } from '../../components/Dropdown/Dropdown';
+import Error from '../../components/Error/Error';
+import Form from '../../components/Form/Form';
+import FormGroup from '../../components/Form/FormGroup/FormGroup';
+import Input from '../../components/Input/Input';
 import { Theme, useTheme } from '../../contexts/ThemeContext';
+import formik from '../../hooks/formik';
+import { useFetchUser } from '../../hooks/user';
 import i18n from '../../i18n/i18n';
 import styles from './SettingsView.module.scss';
+import * as Yup from 'yup';
 
 function SettingsView() {
   const navigate = useNavigate();
   const { t } = useTranslation();
 
   const { setTheme, theme } = useTheme();
+
+  const { update, user, error } = useFetchUser();
 
   const themeOptions: Option[] = [
     { title: t('dark'), value: 'dark' },
@@ -26,34 +36,97 @@ function SettingsView() {
     i18n.changeLanguage(lang, () => window.location.reload());
   };
 
+  const passwordChangeValidationSchema = Yup.object().shape({
+    password: Yup.string()
+      .required(t('input_required') || '')
+      .min(8, t('password_min_chars_error') || ''),
+    passwordRepeat: Yup.string()
+      .required(t('passwords_do_not_match') || '')
+      .oneOf([Yup.ref('password'), null], t('passwords_do_not_match') || ''),
+  });
+
+  const passwordChangeFormik = useFormik({
+    initialValues: {
+      password: '',
+      passwordRepeat: '',
+    },
+    validationSchema: passwordChangeValidationSchema,
+    onSubmit: async ({ password }) => {
+      await update({ password });
+
+      passwordChangeFormik.resetForm();
+    },
+  });
+
+  if (error) {
+    return <Error message={t('unexpected')} />;
+  }
+
   return (
     <div className={styles.Settings}>
-      <div>
-        <Dropdown
-          label={t('appearance')}
-          value={theme}
-          options={themeOptions}
-          onChange={(theme) => setTheme(theme as Theme)}
-          maxWidth="10rem"
-          fullWidth
-        />
+      <div className={styles.heading}>
+        <h1>{t('settings')}</h1>
       </div>
-      <div>
-        <Dropdown
-          label={t('language')}
-          value={i18n.language}
-          options={languageOptions}
-          onChange={handleChangeLanguage}
-          maxWidth="10rem"
-          fullWidth
-        />
-      </div>
-      <div>
-        <Button
-          label={t('logout')}
-          style="red"
-          onClick={() => navigate('/logout')}
-        />
+      <div className={styles.row}>
+        <Form
+          onSubmit={passwordChangeFormik.handleSubmit}
+          className={styles.section}
+        >
+          <Input
+            label={t('password')}
+            value={passwordChangeFormik.values.password}
+            name="password"
+            type="password"
+            onChange={passwordChangeFormik.handleChange}
+            onBlur={passwordChangeFormik.handleBlur}
+            error={
+              passwordChangeFormik.touched.password
+                ? passwordChangeFormik.errors.password
+                : undefined
+            }
+            fullWidth
+          />
+          <Input
+            label={t('password_repeat')}
+            value={passwordChangeFormik.values.passwordRepeat}
+            name="passwordRepeat"
+            type="password"
+            onChange={passwordChangeFormik.handleChange}
+            onBlur={passwordChangeFormik.handleBlur}
+            error={
+              passwordChangeFormik.touched.passwordRepeat
+                ? passwordChangeFormik.errors.passwordRepeat
+                : undefined
+            }
+            fullWidth
+          />
+          <div>
+            <Button type="submit" label={t('save')} />
+          </div>
+        </Form>
+        <div className={styles.section}>
+          <Dropdown
+            label={t('appearance')}
+            value={theme}
+            options={themeOptions}
+            onChange={(theme) => setTheme(theme as Theme)}
+            fullWidth
+          />
+          <Dropdown
+            label={t('language')}
+            value={i18n.language}
+            options={languageOptions}
+            onChange={handleChangeLanguage}
+            fullWidth
+          />
+          <div>
+            <Button
+              label={t('logout')}
+              style="red"
+              onClick={() => navigate('/logout')}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
