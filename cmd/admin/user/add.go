c64c472caa678fdd1915cca50a1f0cea0233cf31
David Borzek <mail@davidborzek.de>
2023-03-19 22:59:45 +0100

    feat: create user via cli non-interactive
    This allows to create a new user via the cli
    by passing the values via flags and/or the password
    via enironment variable.
    
    This is useful for unattended setups.

--- cmd/admin/user/add.go
+++ cmd/admin/user/add.go
@@ -13,8 +13,32 @@ import (
 )
 
 var addUserCmd = &cli.Command{
-	Name:   "add",
-	Usage:  "Add a new user",
+	Name:        "add",
+	Usage:       "Add a new user",
+	Description: "You can use the flags to create a user non-interactive.",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "username",
+			Aliases: []string{"u"},
+			Usage:   "Username of the new user",
+		},
+		&cli.StringFlag{
+			Name:    "password",
+			Aliases: []string{"p"},
+			Usage:   "Password of the new user",
+			EnvVars: []string{"TVHGO_PASSWORD"},
+		},
+		&cli.StringFlag{
+			Name:    "email",
+			Aliases: []string{"e"},
+			Usage:   "Email of the new user",
+		},
+		&cli.StringFlag{
+			Name:    "display-name",
+			Aliases: []string{"n"},
+			Usage:   "Display name of the new user",
+		},
+	},
 	Action: addUser,
 }
 
@@ -44,6 +68,19 @@ var qs = []*survey.Question{
 }
 
 func addUser(ctx *cli.Context) error {
+	if ctx.IsSet("username") &&
+		ctx.IsSet("password") &&
+		ctx.IsSet("email") &&
+		ctx.IsSet("display-name") {
+
+		return createUser(
+			ctx.String("username"),
+			ctx.String("password"),
+			ctx.String("email"),
+			ctx.String("display-name"),
+		)
+	}
+
 	answers := struct {
 		Username    string
 		Password    string
@@ -59,17 +96,26 @@ func addUser(ctx *cli.Context) error {
 		return err
 	}
 
+	return createUser(
+		answers.Username,
+		answers.Password,
+		answers.Email,
+		answers.DisplayName,
+	)
+}
+
+func createUser(username, password, email, displayName string) error {
 	userRepository := user.New(actx.GetDB())
 
-	hash, err := auth.HashPassword(answers.Password)
+	hash, err := auth.HashPassword(password)
 	if err != nil {
 		return err
 	}
 
 	err = userRepository.Create(context.Background(), &core.User{
-		Username:     answers.Username,
-		Email:        answers.Email,
-		DisplayName:  answers.DisplayName,
+		Username:     username,
+		Email:        email,
+		DisplayName:  displayName,
 		PasswordHash: string(hash),
 	})
 	if err != nil {
