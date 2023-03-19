d55d91e84e5e351230ef34b34fb1a99d0e0793bf
David Borzek <mail@davidborzek.de>
2023-03-19 16:06:33 +0100

    fix: keep ui/dist folder in vsc to allow go build without ui build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { env } from "process";
+import { openSync } from "fs";
+import { resolve } from "path";
 
 const commitHash = env.GIT_COMMIT || "local";
 const version = env.VERSION || "local";
+
+// This plugin creates a keep file to include the
+// dist directory to the version control but exclude the content.
+const keep = {
+  name: "Create static keep file for git",
+  closeBundle() {
+    console.error("HALLO");
+    
+    openSync(resolve(__dirname, 'dist/keep'), "w");
+}};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +32,6 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    keep,
   ],
 });
