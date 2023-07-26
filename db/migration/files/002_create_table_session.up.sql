db21b6bd19cde9128b9e789eb4f225b6c7c8a9e1
David Borzek <mail@davidborzek.de>
2023-07-27 00:43:51 +0200

    fix: session table has unique token on delete cascade
    This cannot be done with migration.

--- db/migration/files/002_create_table_session.up.sql
+++ db/migration/files/002_create_table_session.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS session (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    hashed_token TEXT NOT NULL,
+    hashed_token TEXT UNIQUE NOT NULL,
     client_ip TEXT NOT NULL,
     user_agent TEXT NOT NULL,
     created_at INTEGER NOT NULL,
     last_used_at INTEGER,
     rotated_at INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE
 )
