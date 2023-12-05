cb2c1f8c7ac7314b4070cc15888f954a96fd4288
David Borzek <mail@davidborzek.de>
2023-12-05 23:10:00 +0100

    chore: ui test setup using vitest

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -33,6 +34,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
   },
   plugins: [react(), svgr(), keep],
 });
