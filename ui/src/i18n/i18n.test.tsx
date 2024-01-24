f3fd36ed39d9692047e02e1a7eee8cbc3cc62775
David Borzek <mail@davidborzek.de>
2024-01-24 23:46:25 +0100

    test: add i18n initialize tests

---
+++ ui/src/i18n/i18n.test.tsx
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest';
+import i18n, { t } from 'i18next';
+
+test('can initialize i18n', async () => {
+  // set prod to true, to reduce logs.
+  import.meta.env.PROD = true;
+
+  await import('./i18n');
+
+  expect(i18n.isInitialized).toBeTruthy();
+  expect(t("username")).toEqual("Username");
+});
