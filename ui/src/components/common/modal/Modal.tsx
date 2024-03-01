d704c84bf375c2120b0dffbcd9605915c7fd3fef
David Borzek <mail@davidborzek.de>
2024-03-01 14:19:52 +0100

    refactor: modal component

--- ui/src/components/common/modal/Modal.tsx
+++ ui/src/components/common/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import styles from './Modal.module.scss';
 import { PropsWithChildren, useEffect, useRef } from 'react';
 import ModalCloseButton from './ModalCloseButton';
+import { c } from '@/utils/classNames';
 
 type Props = {
   visible: boolean;
@@ -24,17 +25,17 @@ export default function Modal(props: PropsWithChildren<Props>) {
     };
   }, []);
 
+  const handleClose = (event: React.MouseEvent<HTMLInputElement>) => {
+    if (event.target === ref.current && !props.disableBackdropClose) {
+      props.onClose();
+    }
+  };
+
   return (
     <div
       ref={ref}
-      className={`${styles.modalWrapper} ${
-        props.visible ? styles.visible : ''
-      }`}
-      onClick={(event) => {
-        if (event.target === ref.current) {
-          !props.disableBackdropClose && props.onClose();
-        }
-      }}
+      className={c(styles.wrapper, props.visible ? styles.visible : undefined)}
+      onClick={handleClose}
     >
       <div className={styles.modal} style={{ maxWidth: props.maxWidth }}>
         <div className={styles.container}>
