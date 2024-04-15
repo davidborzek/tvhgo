35b45719a6357fae9edbaf8f5d2a5b468658014b
David Borzek <mail@davidborzek.de>
2024-04-15 16:36:01 +0200

    fix: auth provider rerender loop

--- ui/src/hooks/notification.ts
+++ ui/src/hooks/notification.ts
@@ -1,19 +1,26 @@
 import { toast } from 'react-toastify';
+import { useCallback } from 'react';
 
 export const useNotification = (notificationId: string) => {
-  const notifyError = (message?: string | null) => {
-    toast.error(message, {
-      toastId: notificationId,
-      updateId: notificationId,
-    });
-  };
+  const notifyError = useCallback(
+    (message?: string | null) => {
+      toast.error(message, {
+        toastId: notificationId,
+        updateId: notificationId,
+      });
+    },
+    [notificationId]
+  );
 
-  const notifySuccess = (message?: string | null) => {
-    toast.success(message, {
-      toastId: notificationId,
-      updateId: notificationId,
-    });
-  };
+  const notifySuccess = useCallback(
+    (message?: string | null) => {
+      toast.success(message, {
+        toastId: notificationId,
+        updateId: notificationId,
+      });
+    },
+    [notificationId]
+  );
 
   const dismissNotification = () => toast.dismiss(notificationId);
 
